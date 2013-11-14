531097feb3b73a90a45cc9c6cf1f52463b797574
Juan Pablo <tinkertrain@gmail.com>
2013-11-14 16:40:57 -0600

    Footer contact menu

---
+++ menu-contact.php
@@ -0,0 +1,8 @@
+<nav class="contact" role="navigation">
+	<ul>
+			<li><a class="icon-twitter" href="http://twitter.com/thetinkertrain" aria-label="Twitter"><span>Twitter</span></a></li><!--
+		 --><li><a class="icon-github" href="http://github.com/tinkertrain" aria-label="Github"><span>Github</span></a></li><!--
+		 --><li><a class="icon-linkedin" href="http://linkedin.com/jplomeli" aria-label="Linkedin"><span>Linkedin</span></a></li><!--
+		 --><li><a class="icon-envelope-alt" href="mailto:juanpablo@thetinkertrain.com" aria-label="Email"><span>Email</span></a></li>
+	</ul>
+</nav>
