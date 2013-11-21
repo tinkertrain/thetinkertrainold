47dca543c1a40f75fd55aef49e87ba279a8dcf14
Juan Pablo <tinkertrain@gmail.com>
2013-11-21 15:50:05 -0600

    Added markup for the project details and send it via ajax

--- work.php
+++ work.php
@@ -5,8 +5,76 @@
  *
  */
 
-include("./header.php");
+
+$testing = false;
 ?>
+
+<?php if ($testing || $config->ajax) : ?>
+  <?php
+    $projectReceived = $_POST["projectId"];
+    $title = $page->projects[$projectReceived]->project_title;
+    $involvement = $page->projects[$projectReceived]->project_involvement;
+    $details = $page->projects[$projectReceived]->project_details;
+    $logo = $page->projects[$projectReceived]->project_logo;
+    $tech = $page->projects[$projectReceived]->project_tech;
+    $url = $page->projects[$projectReceived]->project_url;
+    $screenshots = $page->projects[$projectReceived]->project_screenshots;
+    $demo = $page->projects[$projectReceived]->project_demo;
+   ?>
+
+<div class="project-detail">
+  <button class="close">&times;</button>
+  <section class="details">
+    <h2>Project</h2>
+    <div>
+      <h3>
+        <?php
+          if ($url) echo "<a href='$url'>$title</a>";
+          elseif ($demo) echo "<a href='$demo'>$title</a>";
+          else  echo $title;
+        ?>
+      </h3>
+      <p><?= $details ?></p>
+    </div><!--
+    --><img src="<?= $logo->url ?>" alt="Project logo">
+  </section>
+
+  <section class="involvement">
+    <h2>Involvement</h2>
+    <p><?= $involvement ?></p>
+  </section>
+
+  <div>
+    <section class="tech">
+      <h2>Tech</h2>
+      <div><?= $tech ?></div>
+    </section>
+    <?php if($screenshots): ?>
+      <section class="screenshots">
+        <h2>Screenshots</h2>
+        <ul>
+          <?php foreach($screenshots as $screen): ?>
+            <?php
+              $options = array(
+                'quality' => 90,
+                'upscaling' => false
+              );
+              $thumb =  $screen->size(50, 50, $options);
+            ?>
+          <li>
+            <a href="<?= $screen->url ?>" target="_blank">
+              <img src="<?= $thumb->url ?>" alt="Screenshot">
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+      </section>
+    <?php endif; ?>
+  </div>
+</div>
+
+<?php else: ?>
+<?php include("./header.php"); ?>
 <section role="main">
 
   <?= $page->body ?>
@@ -36,5 +104,5 @@ include("./header.php");
   </ul>
 </div>
 </section>
-
 <?php include("./footer.php"); ?>
+<?php endif; ?>
