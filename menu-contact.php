f455b5cb851e2a078f91bdc2fbe5ce0dee2e1c41
Juan Pablo <tinkertrain@gmail.com>
2013-11-25 17:51:04 -0600

    Fixed linkedin link@

--- menu-contact.php
+++ menu-contact.php
@@ -2,7 +2,7 @@
 	<ul>
 			<li><a class="icon-twitter" href="http://twitter.com/thetinkertrain" aria-label="Twitter"><span>Twitter</span></a></li><!--
 		 --><li><a class="icon-github" href="http://github.com/tinkertrain" aria-label="Github"><span>Github</span></a></li><!--
-		 --><li><a class="icon-linkedin" href="http://linkedin.com/jplomeli" aria-label="Linkedin"><span>Linkedin</span></a></li><!--
+		 --><li><a class="icon-linkedin" href="http://mx.linkedin.com/pub/juan-pablo-lomeli-diaz/67/3ab/924/" aria-label="Linkedin"><span>Linkedin</span></a></li><!--
 		 --><li><a class="icon-envelope-alt" href="mailto:juanpablo@thetinkertrain.com" aria-label="Email"><span>Email</span></a></li>
 	</ul>
 </nav>
