9ca02f96da70b06420780164172a3c29236bdba0
Juan Pablo <tinkertrain@gmail.com>
2013-11-25 16:27:45 -0600

    Post markup

---
+++ post.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Home template
+ *
+ */
+
+include("./header.php");
+?>
+
+	<section role="main">
+		<div class="switcher">
+			<button class="day"><span>Toggle Day/Night</span></button>
+			<button class="serif">F</button>
+		</div>
+
+		<article role="article" itemscope itemtype="http://schema.org/BlogPosting">
+            <header>
+                <h1><?= $page->title ?></h1>
+                <img src="<?= $page->article_icon->url ?>" alt="Icon" class="post-logo">
+            </header>
+            <article>
+                <?= $page->article ?>
+            </article>
+            <footer role="contentinfo">
+                <time itemprop="dateCreated" datetime="<?= date('Y-m-d', $page->getUnformatted("article_date")) ?>"><?= $page->article_date ?></time>
+            </footer>
+
+        </article>
+	</section>
+
+<?php include("./footer.php") ?>
